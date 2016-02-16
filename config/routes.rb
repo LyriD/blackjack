8284788bfa0e966949ca3f2d12382934cb55453b
lyrs <gvreshetnikov@dasreda.ru>
2016-02-16 17:03:12 +0300

    prototype 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'game#blackjack'
-
-  get 'stake' => 'game#stake', as: :stake
   get 'move/(:method)' => 'game#move', as: :move
 
   # Example of regular route:
